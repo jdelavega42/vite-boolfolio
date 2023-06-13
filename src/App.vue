9e2c60b44447075dd799e08ca85080bdcf8c9c38
“jdelavega94” <jcdelavega94@gmail.com>
2023-06-13 18:45:17 +0200

    front project connected to back end

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import axios from 'axios';
+
+  export default {
+    data(){
+      return{
+        projects: []
+      }
+    },
+    methods: {
+      getProjects() {
+        axios.get(`http://127.0.0.1:8000/api/projects`).then(resp =>{
+          this.projects = resp.data.results;
+        })
+      }
+    },
+    mounted(){
+      this.getProjects();
+    }
+  }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<h1 class="text-center">Front Boolfolio</h1>
+<div class="container">
+  <h2>Lista  dei Progetti</h2>
+  <ul class="row row-cols-3 g-3">
+    <li class="col" v-for="project in projects" :key="project.id">
+      {{ project.title }}
+    </li>
+  </ul>
+</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@use './style/general.scss' as *;
 </style>
